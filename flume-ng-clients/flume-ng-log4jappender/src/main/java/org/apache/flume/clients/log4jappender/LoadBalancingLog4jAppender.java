a32c74a661ad27bd5de9b1299d18f14dcef98a67
zhisheng <tianzhisheng10850@hellobike.com>
2020-02-02 12:57:50 +0800

    添加 flume clients 的注释

--- flume-ng-clients/flume-ng-log4jappender/src/main/java/org/apache/flume/clients/log4jappender/LoadBalancingLog4jAppender.java
+++ flume-ng-clients/flume-ng-log4jappender/src/main/java/org/apache/flume/clients/log4jappender/LoadBalancingLog4jAppender.java
@@ -18,8 +18,6 @@
  */
 package org.apache.flume.clients.log4jappender;
 
-import java.util.Properties;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.flume.FlumeException;
 import org.apache.flume.api.RpcClientConfigurationConstants;
@@ -28,8 +26,9 @@ import org.apache.flume.api.RpcClientFactory.ClientType;
 import org.apache.log4j.helpers.LogLog;
 import org.apache.log4j.spi.LoggingEvent;
 
+import java.util.Properties;
+
 /**
- *
  * Appends Log4j Events to an external Flume client which is decribed by the
  * Log4j configuration file. The appender takes the following required
  * parameters:
@@ -75,106 +74,103 @@ import org.apache.log4j.spi.LoggingEvent;
  * <pre>
  * <p>log4j.logger.com.bar.foo = DEBUG,out2</p>
  * </pre>
- *
- *
  */
 public class LoadBalancingLog4jAppender extends Log4jAppender {
 
-  private String hosts;
-  private String selector;
-  private String maxBackoff;
-  private boolean configured = false;
+    private String hosts;
+    private String selector;
+    private String maxBackoff;
+    private boolean configured = false;
 
-  public void setHosts(String hostNames) {
-    this.hosts = hostNames;
-  }
-
-  public void setSelector(String selector) {
-    this.selector = selector;
-  }
-
-  public void setMaxBackoff(String maxBackoff) {
-    this.maxBackoff = maxBackoff;
-  }
-
-  @Override
-  public synchronized void append(LoggingEvent event) {
-    if (!configured) {
-      String errorMsg = "Flume Log4jAppender not configured correctly! Cannot" +
-          " send events to Flume.";
-      LogLog.error(errorMsg);
-      if (getUnsafeMode()) {
-        return;
-      }
-      throw new FlumeException(errorMsg);
-    }
-    super.append(event);
-  }
-
-  /**
-   * Activate the options set using <tt>setHosts()</tt>, <tt>setSelector</tt>
-   * and <tt>setMaxBackoff</tt>
-   *
-   * @throws FlumeException
-   *           if the LoadBalancingRpcClient cannot be instantiated.
-   */
-  @Override
-  public void activateOptions() throws FlumeException {
-    try {
-      final Properties properties = getProperties(hosts, selector, maxBackoff, getTimeout());
-      rpcClient = RpcClientFactory.getInstance(properties);
-      if (layout != null) {
-        layout.activateOptions();
-      }
-      configured = true;
-    } catch (Exception e) {
-      String errormsg = "RPC client creation failed! " + e.getMessage();
-      LogLog.error(errormsg);
-      if (getUnsafeMode()) {
-        return;
-      }
-      throw new FlumeException(e);
-    }
-    initializeClientAddress();
-  }
-
-  private Properties getProperties(String hosts, String selector,
-      String maxBackoff, long timeout) throws FlumeException {
-
-    if (StringUtils.isEmpty(hosts)) {
-      throw new FlumeException("hosts must not be null");
+    public void setHosts(String hostNames) {
+        this.hosts = hostNames;
     }
 
-    Properties props = new Properties();
-    String[] hostsAndPorts = hosts.split("\\s+");
-    StringBuilder names = new StringBuilder();
-    for (int i = 0; i < hostsAndPorts.length; i++) {
-      String hostAndPort = hostsAndPorts[i];
-      String name = "h" + i;
-      props.setProperty(RpcClientConfigurationConstants.CONFIG_HOSTS_PREFIX + name,
-          hostAndPort);
-      names.append(name).append(" ");
-    }
-    props.put(RpcClientConfigurationConstants.CONFIG_HOSTS, names.toString());
-    props.put(RpcClientConfigurationConstants.CONFIG_CLIENT_TYPE,
-        ClientType.DEFAULT_LOADBALANCE.toString());
-    if (!StringUtils.isEmpty(selector)) {
-      props.put(RpcClientConfigurationConstants.CONFIG_HOST_SELECTOR, selector);
+    public void setSelector(String selector) {
+        this.selector = selector;
     }
 
-    if (!StringUtils.isEmpty(maxBackoff)) {
-      long millis = Long.parseLong(maxBackoff.trim());
-      if (millis <= 0) {
-        throw new FlumeException(
-            "Misconfigured max backoff, value must be greater than 0");
-      }
-      props.put(RpcClientConfigurationConstants.CONFIG_BACKOFF, String.valueOf(true));
-      props.put(RpcClientConfigurationConstants.CONFIG_MAX_BACKOFF, maxBackoff);
+    public void setMaxBackoff(String maxBackoff) {
+        this.maxBackoff = maxBackoff;
     }
-    props.setProperty(RpcClientConfigurationConstants.CONFIG_CONNECT_TIMEOUT,
-                      String.valueOf(timeout));
-    props.setProperty(RpcClientConfigurationConstants.CONFIG_REQUEST_TIMEOUT,
-                      String.valueOf(timeout));
-    return props;
-  }
+
+    @Override
+    public synchronized void append(LoggingEvent event) {
+        if (!configured) {
+            String errorMsg = "Flume Log4jAppender not configured correctly! Cannot" +
+                    " send events to Flume.";
+            LogLog.error(errorMsg);
+            if (getUnsafeMode()) {
+                return;
+            }
+            throw new FlumeException(errorMsg);
+        }
+        super.append(event);
+    }
+
+    /**
+     * Activate the options set using <tt>setHosts()</tt>, <tt>setSelector</tt>
+     * and <tt>setMaxBackoff</tt>
+     *
+     * @throws FlumeException if the LoadBalancingRpcClient cannot be instantiated.
+     */
+    @Override
+    public void activateOptions() throws FlumeException {
+        try {
+            final Properties properties = getProperties(hosts, selector, maxBackoff, getTimeout());
+            rpcClient = RpcClientFactory.getInstance(properties);
+            if (layout != null) {
+                layout.activateOptions();
+            }
+            configured = true;
+        } catch (Exception e) {
+            String errormsg = "RPC client creation failed! " + e.getMessage();
+            LogLog.error(errormsg);
+            if (getUnsafeMode()) {
+                return;
+            }
+            throw new FlumeException(e);
+        }
+        initializeClientAddress();
+    }
+
+    private Properties getProperties(String hosts, String selector,
+                                     String maxBackoff, long timeout) throws FlumeException {
+
+        if (StringUtils.isEmpty(hosts)) {
+            throw new FlumeException("hosts must not be null");
+        }
+
+        Properties props = new Properties();
+        String[] hostsAndPorts = hosts.split("\\s+");
+        StringBuilder names = new StringBuilder();
+        for (int i = 0; i < hostsAndPorts.length; i++) {
+            String hostAndPort = hostsAndPorts[i];
+            String name = "h" + i;
+            props.setProperty(RpcClientConfigurationConstants.CONFIG_HOSTS_PREFIX + name,
+                    hostAndPort);
+            names.append(name).append(" ");
+        }
+        props.put(RpcClientConfigurationConstants.CONFIG_HOSTS, names.toString());
+        props.put(RpcClientConfigurationConstants.CONFIG_CLIENT_TYPE,
+                ClientType.DEFAULT_LOADBALANCE.toString());
+        if (!StringUtils.isEmpty(selector)) {
+            props.put(RpcClientConfigurationConstants.CONFIG_HOST_SELECTOR, selector);
+        }
+
+        if (!StringUtils.isEmpty(maxBackoff)) {
+            long millis = Long.parseLong(maxBackoff.trim());
+            if (millis <= 0) {
+                throw new FlumeException(
+                        "Misconfigured max backoff, value must be greater than 0");
+            }
+            props.put(RpcClientConfigurationConstants.CONFIG_BACKOFF, String.valueOf(true));
+            props.put(RpcClientConfigurationConstants.CONFIG_MAX_BACKOFF, maxBackoff);
+        }
+        props.setProperty(RpcClientConfigurationConstants.CONFIG_CONNECT_TIMEOUT,
+                String.valueOf(timeout));
+        props.setProperty(RpcClientConfigurationConstants.CONFIG_REQUEST_TIMEOUT,
+                String.valueOf(timeout));
+        return props;
+    }
 }
--- flume-ng-clients/flume-ng-log4jappender/src/main/java/org/apache/flume/clients/log4jappender/Log4jAvroHeaders.java
+++ flume-ng-clients/flume-ng-log4jappender/src/main/java/org/apache/flume/clients/log4jappender/Log4jAvroHeaders.java
@@ -21,36 +21,37 @@ package org.apache.flume.clients.log4jappender;
 import java.util.Locale;
 
 public enum Log4jAvroHeaders {
-  OTHER("flume.client.log4j.logger.other"),
-  LOGGER_NAME("flume.client.log4j.logger.name"),
-  LOG_LEVEL("flume.client.log4j.log.level"),
-  MESSAGE_ENCODING("flume.client.log4j.message.encoding"),
-  ADDRESS("flume.client.log4j.address"),
-  TIMESTAMP("flume.client.log4j.timestamp"),
-  AVRO_SCHEMA_LITERAL("flume.avro.schema.literal"),
-  AVRO_SCHEMA_URL("flume.avro.schema.url");
+    OTHER("flume.client.log4j.logger.other"),
+    LOGGER_NAME("flume.client.log4j.logger.name"),
+    LOG_LEVEL("flume.client.log4j.log.level"),
+    MESSAGE_ENCODING("flume.client.log4j.message.encoding"),
+    ADDRESS("flume.client.log4j.address"),
+    TIMESTAMP("flume.client.log4j.timestamp"),
+    AVRO_SCHEMA_LITERAL("flume.avro.schema.literal"),
+    AVRO_SCHEMA_URL("flume.avro.schema.url");
 
-  private String headerName;
-  private Log4jAvroHeaders(String headerName) {
-    this.headerName = headerName;
-  }
+    private String headerName;
 
-  public String getName() {
-    return headerName;
-  }
-
-  public String toString() {
-    return getName();
-  }
-
-  public static Log4jAvroHeaders getByName(String headerName) {
-    Log4jAvroHeaders hdrs = null;
-    try {
-      hdrs = Log4jAvroHeaders.valueOf(headerName.toLowerCase(Locale.ENGLISH).trim());
-    } catch (IllegalArgumentException e) {
-      hdrs = Log4jAvroHeaders.OTHER;
+    private Log4jAvroHeaders(String headerName) {
+        this.headerName = headerName;
     }
-    return hdrs;
-  }
+
+    public String getName() {
+        return headerName;
+    }
+
+    public String toString() {
+        return getName();
+    }
+
+    public static Log4jAvroHeaders getByName(String headerName) {
+        Log4jAvroHeaders hdrs = null;
+        try {
+            hdrs = Log4jAvroHeaders.valueOf(headerName.toLowerCase(Locale.ENGLISH).trim());
+        } catch (IllegalArgumentException e) {
+            hdrs = Log4jAvroHeaders.OTHER;
+        }
+        return hdrs;
+    }
 
 }
